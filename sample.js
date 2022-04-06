acaba3d38b0fd09ac8c39755df6efc31052f4d15
TomitaNok <tomitanok@sohobb.jp>
2022-04-06 17:31:11 +0900

    Step5

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 const tasksList = [];
 
 const onClickButton = () => {
-  const todoList = document.querySelector("#todo-list");
+  const tableBody = document.querySelector("#table-body");
   const name = document.querySelector("#name");
   const task = document.querySelector("#task");
 
@@ -9,28 +9,18 @@ const onClickButton = () => {
   tasksList.forEach((todo, idx) =>
     console.log(`${idx}: ${todo.name}, ${todo.task}`)
   );
-  const tbl = generateTable();
-
-  todoList.childNodes.forEach((child) => {
-    todoList.removeChild(child);
-  });
-  todoList.appendChild(tbl);
+  generateTableData(tableBody, ["name", "task"]);
 };
 
-const generateTable = () => {
-  const tbl = document.createElement("table");
-  const tblBody = document.createElement("tbody");
-
-  tasksList.forEach((task) => {
-    const row = document.createElement("tr");
-    Object.values(task).forEach((value) => {
-      const cell = document.createElement("td");
-      const cellText = document.createTextNode(value);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    });
-    tblBody.appendChild(row);
+const generateTableData = (tableBody, queryKeys) => {
+  const row = document.createElement("tr");
+  queryKeys.forEach((key) => {
+    const cell = document.createElement("td");
+    const cellText = document.createTextNode(
+      tasksList[tasksList.length - 1][key]
+    );
+    cell.appendChild(cellText);
+    row.appendChild(cell);
   });
-  tbl.appendChild(tblBody);
-  return tbl;
+  return tableBody.appendChild(row);
 };
