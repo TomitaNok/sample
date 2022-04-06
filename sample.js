04d0d2c6deb1c35304594001b871b87bd07fe00d
TomitaNok <tomitanok@sohobb.jp>
2022-04-06 17:27:51 +0900

    Step2

---
+++ sample.js
@@ -0,0 +1,6 @@
+const onClickButton = () => {
+    const name = document.querySelector("#name");
+    const task = document.querySelector("#task");
+    console.log(name.value, task.value);
+  };
+  
