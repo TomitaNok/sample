6f6c0c3ec776deb508446737ca42db4b3168a708
TomitaNok <tomitanok@sohobb.jp>
2022-04-06 17:28:57 +0900

    Step3

--- sample.js
+++ sample.js
@@ -1,6 +1,9 @@
+const tasksList = [];
+
 const onClickButton = () => {
-    const name = document.querySelector("#name");
-    const task = document.querySelector("#task");
-    console.log(name.value, task.value);
-  };
-  
+  const name = document.querySelector("#name");
+  const task = document.querySelector("#task");
+
+  tasksList.push({ name: name.value, task: task.value });
+  tasksList.forEach((t, idx) => console.log(`${idx}: ${t.name}, ${t.task}`));
+};
