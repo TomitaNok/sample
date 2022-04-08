f906bf86b532ca992cba6471c75093944d0a11a1
HarunobuTsukamoto <tsukamotohrnb@sohobb.jp>
2022-04-08 10:05:54 +0900

    データ登録後データベースと同期してテーブル更新

--- sample.js
+++ sample.js
@@ -7,26 +7,18 @@ const onClickButton = () => {
   const name = document.querySelector("#name");
   const task = document.querySelector("#task");
 
-  let tasks = {
+  const tasks = {
     uname: name.value,
     title: task.value
   }
 
   // webAPIを利用してタスクをデータベースに登録
-  createNewTask(tasks);
-
-  // tasksListにタスクを追加
-  tasksList.push({ uname: name.value, title: task.value });
-  tasksList.forEach((todo, idx) =>
-    console.log(`${idx}: ${todo.name}, ${todo.task}`)
-  );
-
-  // テーブルに新しいタスクを追加
-  generateTableData(tableBody, ["uname", "title"]);
+  // タスク登録後データベースと同期してテーブルを更新
+  createNewTask(tasks).then(() => syncTaskDatabase());
 };
 
 // 取得ボタンクリック時の動作
-const onClickGetButton = async () => {
+const syncTaskDatabase = async () => {
   const tableBody = document.querySelector("#table-body");
 
   // webAPIを利用してデータベース内のタスクを取得しtasksListの中身を入れ替える
@@ -34,20 +26,6 @@ const onClickGetButton = async () => {
 
   // テーブルの更新
   generateAllTableData(tableBody, ["uname", "title"]);
-};
-
-// 登録ボタン押下時のテーブル更新
-const generateTableData = (tableBody, queryKeys) => {
-  const row = document.createElement("tr");
-  queryKeys.forEach((key) => {
-    const cell = document.createElement("td");
-    const cellText = document.createTextNode(
-      tasksList[tasksList.length - 1][key]
-    );
-    cell.appendChild(cellText);
-    row.appendChild(cell);
-  });
-  return tableBody.appendChild(row);
 };
 
 // 取得ボタン押下時のテーブル更新
