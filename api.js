057b482f860222f1e0b658e54aabee968437ed16
HarunobuTsukamoto <tsukamotohrnb@sohobb.jp>
2022-04-08 09:35:22 +0900

    タスクの登録と全タスク取得のwebAPIサンプル作成

---
+++ api.js
@@ -0,0 +1,94 @@
+const HOST_URI = "http://localhost:2022/tasks";
+
+class APIConnecter {
+  constructor(host, method) {
+    this.host = host;
+    this.method = method;
+    switch (this.method) {
+      case "GET":
+        this.request = (url) =>
+          fetch(`${this.host}/${url}`, {
+            method: this.method,
+          })
+            .then((responce) => responce.json())
+            .catch((e) => {
+              console.error(e);
+              return [];
+            });
+        break;
+      case "POST":
+        this.request = (url, body) =>
+          fetch(`${this.host}/${url}`, {
+            method: this.method,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+          })
+            .then((responce) => responce.json())
+            .catch((e) => {
+              console.error(e);
+              return [];
+            });
+        break;
+      case "PATCH":
+        this.request = (url, body) =>
+          fetch(`${this.host}/${url}`, {
+            method: this.method,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+          })
+            .then((responce) => responce.json())
+            .catch((e) => {
+              console.error(e);
+              return [];
+            });
+        break;
+      case "DELETE":
+        this.request = (url) =>
+          fetch(`${this.host}/${url}`, {
+            method: this.method,
+          })
+            .then((responce) => responce.json())
+            .catch((e) => {
+              console.error(e);
+              return [];
+            });
+        break;
+    }
+  }
+}
+
+const createNewTask = (body) => {
+  const api = new APIConnecter(HOST_URI, "POST");
+  // const body = { uname: "hoge", title: "fuga", completed: false };
+  return api.request("", body);
+};
+
+const getAllTasks = async () => {
+  const api = new APIConnecter(HOST_URI, "GET");
+  const tasks = await api.request("");
+  return tasks;
+};
+const getTaskById = async (id) => {
+  const api = new APIConnecter(HOST_URI, "GET");
+  const task = await api.request(`${id}`);
+  console.log(task);
+  return task;
+};
+
+const updateTask = (id, body) => {
+  const api = new APIConnecter(HOST_URI, "PATCH");
+  return api.request(`${id}`, body);
+};
+
+const removeTask = (id) => {
+  const api = new APIConnecter(HOST_URI, "DELETE");
+  return api.request(`${id}`);
+};
+
+const printConsole = (tasks) => {
+  tasks.forEach((t) =>
+    console.log(
+      `uname: ${t.uname}\ntitle: ${t.title}\ncreatedAi: ${t.createdAt}`
+    )
+  );
+};
--- sample.js
+++ sample.js
@@ -1,17 +1,41 @@
-const tasksList = [];
+let tasksList = [];
 
+// 登録ボタンクリック時の動作
 const onClickButton = () => {
   const tableBody = document.querySelector("#table-body");
   const name = document.querySelector("#name");
   const task = document.querySelector("#task");
 
-  tasksList.push({ name: name.value, task: task.value });
+  let tasks = {
+    uname: name.value,
+    title: task.value
+  }
+
+  // webAPIを利用してタスクをデータベースに登録
+  createNewTask(tasks);
+
+  // tasksListにタスクを追加
+  tasksList.push({ uname: name.value, title: task.value });
   tasksList.forEach((todo, idx) =>
     console.log(`${idx}: ${todo.name}, ${todo.task}`)
   );
-  generateTableData(tableBody, ["name", "task"]);
+
+  // テーブルに新しいタスクを追加
+  generateTableData(tableBody, ["uname", "title"]);
 };
 
+// 取得ボタンクリック時の動作
+const onClickGetButton = async () => {
+  const tableBody = document.querySelector("#table-body");
+
+  // webAPIを利用してデータベース内のタスクを取得しtasksListの中身を入れ替える
+  tasksList = await getAllTasks();
+
+  // テーブルの更新
+  generateAllTableData(tableBody, ["uname", "title"]);
+};
+
+// 登録ボタン押下時のテーブル更新
 const generateTableData = (tableBody, queryKeys) => {
   const row = document.createElement("tr");
   queryKeys.forEach((key) => {
@@ -23,4 +47,24 @@ const generateTableData = (tableBody, queryKeys) => {
     row.appendChild(cell);
   });
   return tableBody.appendChild(row);
+};
+
+// 取得ボタン押下時のテーブル更新
+const generateAllTableData = (tableBody, queryKeys) => {
+  // テーブルボディを空にする
+  tableBody.innerHTML = "";
+
+  // webAPIで取得したtasksListの中身をすべてテーブルに追加
+  tasksList.forEach((task) => {
+    const row = document.createElement("tr");
+    queryKeys.forEach((key) => {
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(
+        task[key]
+      );
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    });
+    tableBody.appendChild(row);
+  });
 };
