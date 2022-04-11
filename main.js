21a9039ff2553c60ac4e44691a530f1c2eedd6d2
TomitaNok <tomitanok@sohobb.jp>
2022-04-11 10:12:18 +0900

    wip

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ let tasksList = [];
 const generateTableData = (tableBody, queryKeys) => {
   tasksList.forEach((task) => {
     const row = document.createElement("tr");
+    row.addEventListener(
+      "click",
+      () => {
+        console.log(task._id);
+      },
+      false
+    );
+    // row.addEventListener("click", generateDetail(task._id), false);
     queryKeys.forEach((key) => {
       const cell = document.createElement("td");
       const cellText = document.createTextNode(task[key]);
       cell.appendChild(cellText);
       row.appendChild(cell);
-      if(key == "priority") {
-        row.setAttribute("class", task[key])
+      if (key == "priority") {
+        row.setAttribute("class", task[key]);
       }
     });
     tableBody.appendChild(row);
@@ -28,4 +36,20 @@ const syncTasks = async () => {
     "completed",
   ]);
   // createNewTask({name: "piyo", title: "ぴよぴよ", desctiption: "ぴよよー", priority: "Top"})
+};
+
+/**
+ *
+ * @param {string} _id
+ * @returns {HTMLParagraphElement}
+ */
+const generateDetail = (_id) => {
+  const article = document.querySelector("#page-body");
+
+  const task = tasksList.find((task) => task._id == _id);
+  const p = document.createElement("p");
+  const text = document.createTextNode(task.description);
+  p.appendChild(text);
+
+  article.replaceWith(p);
 };
