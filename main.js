584db767107ee17137b743083568b2d3573d31a8
TomitaNok <tomitanok@sohobb.jp>
2022-04-11 17:42:58 +0900

    wip: Add lorem picsum

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+/**
+ * @type {Task[]}
+ * @typedef {Object} Task
+ * @property {string} _id
+ * @property {string} uname
+ * @property {string} title
+ * @property {string} description
+ * @property {string} priority
+ * @property {boolean} completed
+ * @property {Date} createdAt
+ */
 let tasksList = [];
 
-const generateTableData = (tableBody, queryKeys) => {
+const generateTableData = (tableBody, queryKeys, profs) => {
   tasksList.forEach((task) => {
     const row = document.createElement("tr");
     row.addEventListener(
@@ -19,6 +30,17 @@ const generateTableData = (tableBody, queryKeys) => {
       if (key == "priority") {
         row.setAttribute("class", task[key]);
       }
+      if (key == "uname") {
+        const responce = profs.get(task[key]);
+        const icon = document.createElement("td");
+        const img = document.createElement("img");
+        img.setAttribute("class", "icon");
+        responce.then((res) => {
+          img.setAttribute("src", res.url);
+        });
+        icon.appendChild(img);
+        row.appendChild(icon);
+      }
     });
     tableBody.appendChild(row);
   });
@@ -27,14 +49,13 @@ const generateTableData = (tableBody, queryKeys) => {
 const syncTasks = async () => {
   tasksList = await getAllTasks();
   console.log(tasksList);
+  const profs = await setIcon("uname");
   const tableBody = document.querySelector("#table-body");
-  generateTableData(tableBody, [
-    "title",
-    "uname",
-    "priority",
-    "createdAt",
-    "completed",
-  ]);
+  generateTableData(
+    tableBody,
+    ["title", "uname", "priority", "createdAt", "completed"],
+    profs
+  );
   // createNewTask({name: "piyo", title: "ぴよぴよ", desctiption: "ぴよよー", priority: "Top"})
 };
 
@@ -52,4 +73,17 @@ const generateDetail = (_id) => {
   p.appendChild(text);
 
   article.replaceWith(p);
+};
+
+const setIcon = async (key) => {
+  const profs = tasksList.reduce((map, cur) => {
+    if (!map.has(cur[key])) {
+      map.set(cur[key], fetch(`https://picsum.photos/64`));
+    }
+    return map;
+  }, new Map());
+
+  await Promise.all(profs.values());
+
+  return profs;
 };
